2987bbd276dec7070a98871e0137ff6b8ce1b65a
Cheryl Brady <cbrady80@byui.edu>
2024-04-02 18:18:14 -0600

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertiesComponent } from './properties/properties.component';
+import { TenantsComponent } from './tenants/tenants.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/properties', pathMatch: 'full' },
+  { path: 'properties', component: PropertiesComponent},
+  { path: 'tenants', component: TenantsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
